7b99eddf789faa7321f43da2b0d24556ae576417
Vincent <vincent.martanu@binus.ac.id>
2026-04-05 10:08:25 +0700

    Initialize Project

---
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -0,0 +1,39 @@
+package com.example.demo.Student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/student")
+    public List<Student> GetStudent() {
+        return studentService.GetStudent();
+    }
+
+    @PostMapping("/student")
+    public Student AddStudent(@RequestBody Student student) {
+        return studentService.SaveStudent(student);
+    }
+
+    @PutMapping("/student/{id}")
+    public Student UpdateStudent(@PathVariable Long id, @RequestBody Student student) {
+        return studentService.UpdateStudent(id, student);
+    }
+
+    @DeleteMapping("/student/{id}")
+    public String DeleteStudent(@PathVariable Long id) {
+        studentService.DeleteStudent(id);
+        return "Delete Completed";
+    }
+}
---
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -0,0 +1,54 @@
+package com.example.demo.Student;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> GetStudent() {
+        return (List<Student>) studentRepository.findAll();
+    }
+
+    public Student SaveStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student UpdateStudent(Long id, Student student) {
+
+        Student StudentDB = studentRepository.findById(id).orElse(null);
+
+        assert StudentDB != null;
+
+        if (Objects.nonNull(student.getName()) && !"".equalsIgnoreCase(student.getName())) {
+            StudentDB.setName(student.getName());
+        }
+
+        if (Objects.nonNull(student.getEmail()) && !"".equalsIgnoreCase(student.getEmail())) {
+            StudentDB.setEmail(student.getEmail());
+        }
+
+        if (Objects.nonNull(student.getDob())) {
+            StudentDB.setDob(student.getDob());
+        }
+
+        if (Objects.nonNull(student.getAge())) {
+            StudentDB.setAge(student.getAge());
+        }
+
+        return studentRepository.save(StudentDB);
+    }
+
+    public void DeleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
